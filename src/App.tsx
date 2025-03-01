a32ab653ed4379ba82d5717c4ee136babd22d048
drew08t <drew08@gmail.com>
2025-03-01 09:49:46 -0800

    Turn off testing from frontend for now

--- src/App.tsx
+++ src/App.tsx
@@ -22,36 +22,36 @@ function App() {
     console.log(agencies);
     let agencyCount = 0;
     const totalAgenciesToTest = 5;
-    agencies.map((agency) => {
-      agencyCount += 1;
-      // examples to check approach from doge.gov
-      // title 1, chapter 3: Administrative Conference of the United States - 12.36k words
-      // title 1, chapter IV, part 425: President's Commission on White House Fellowships - 1.43k words
-      // TODO querying on chapter doesn't appear to be working!
+    // agencies.map((agency) => {
+    //   agencyCount += 1;
+    //   // examples to check approach from doge.gov
+    //   // title 1, chapter 3: Administrative Conference of the United States - 12.36k words
+    //   // title 1, chapter IV, part 425: President's Commission on White House Fellowships - 1.43k words
+    //   // TODO querying on chapter doesn't appear to be working!
 
-      agency.cfr_references.map((reference) => {
-        let countQueryArguments = "?date=2025-02-01&";
-        Object.keys(reference).map((key) => {
-          const value = reference[key as keyof cfr_reference];
-          countQueryArguments += `${key}=${value}&`;
-        });
-        countQueryArguments = countQueryArguments.substring(
-          0,
-          countQueryArguments.length - 1
-        );
-        if (agencyCount <= totalAgenciesToTest) {
-          fetch(`/count${countQueryArguments}`, {})
-            .then((res) => res.json())
-            .then((data) => {
-              console.log({
-                agency: agency.display_name,
-                count: data.count,
-                args: countQueryArguments,
-              });
-            });
-        }
-      });
-    });
+    //   agency.cfr_references.map((reference) => {
+    //     let countQueryArguments = "?date=2025-02-01&";
+    //     Object.keys(reference).map((key) => {
+    //       const value = reference[key as keyof cfr_reference];
+    //       countQueryArguments += `${key}=${value}&`;
+    //     });
+    //     countQueryArguments = countQueryArguments.substring(
+    //       0,
+    //       countQueryArguments.length - 1
+    //     );
+    //     if (agencyCount <= totalAgenciesToTest) {
+    //       fetch(`/count${countQueryArguments}`, {})
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //           console.log({
+    //             agency: agency.display_name,
+    //             count: data.count,
+    //             args: countQueryArguments,
+    //           });
+    //         });
+    //     }
+    //   });
+    // });
   }, [agencies]);
 
   return (
